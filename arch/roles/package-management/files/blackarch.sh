81ed80ecf84515595dc314eac0fc30edfa579f69
asdf <asdf@asdf.com>
2024-07-02 20:10:05 +0100

    join scripts

--- arch/roles/package-management/files/blackarch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/env/bin bash
-
-# Enable multilib repository
-
-curl -s https://blackarch.org/strap.sh -o /tmp/strap.sh
-chmod +x /tmp/strap.sh
-sudo /tmp/strap.sh
--- arch/roles/package-management/files/pkg-mng.sh
+++ arch/roles/package-management/files/pkg-mng.sh
@@ -2,6 +2,12 @@
 
 DIR=$HOME/git/paru
 
+# Paru
 mkdir -p $DIR
 git clone https://aur.archlinux.org/paru-bin.git $DIR
 cd $DIR && makepkg -si --noconfirm
+
+# BlackArch
+curl -s https://blackarch.org/strap.sh -o /tmp/strap.sh
+chmod +x /tmp/strap.sh
+sudo /tmp/strap.sh
